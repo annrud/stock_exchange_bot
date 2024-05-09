32390173ab94a26c7c49b84b10fddb99957a14ef
annrud <rudina_anna88@mail.ru>
2024-05-09 22:17:25 +0800

    fix errors

--- app/user/models.py
+++ app/user/models.py
@@ -1,25 +1,25 @@
 import importlib
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.store.database.sqlalchemy_base import BaseModel
 
-importlib.import_module("app.game.models")
+models = importlib.import_module("app.game.models")
 
 
 class User(BaseModel):
     __tablename__ = "user"
-
 
     id: Mapped[int] = mapped_column(primary_key=True)
     first_name: Mapped[str] = mapped_column()
     last_name: Mapped[str] = mapped_column(default=None)
     username: Mapped[str] = mapped_column(unique=True)
 
-    games: Mapped[list["GameUser"]] = relationship(
+    games: Mapped[list["models.GameUser"]] = relationship(
         "GameUser",
         back_populates="user",
     )
 
-    stocks: Mapped[list["UserStock"]] = relationship(
+    stocks: Mapped[list["models.UserStock"]] = relationship(
         "UserStock", back_populates="user"
     )
